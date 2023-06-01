45d1ad77ff6bd921f590ce7523f93c92c130a97d
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2023-06-01 16:51:12 +0200

    better page

--- src/routes/iapau/+page.svelte
+++ src/routes/iapau/+page.svelte
@@ -1,1 +1,24 @@
-<p>iapau</p>
+<div
+  style="background-position: center bottom 0px;
+  background-size: cover;
+background-image: radial-gradient(circle at top right,rgba(255,255,255,0) 20%,#0e8769 100%),url(/img/discover_us.jpg) !important;height: 400px;  background-position: 50%;"
+  class="column-center"
+>
+  <div
+    style="flex: 1; align-items: start !important; width: 55%; color:white;"
+    class="column-center"
+  >
+    <h3 style="opacity: 0.8;">Une équipe, une passion, une aventure</h3>
+    <h1 style="font-size: 3rem; margin: 0px; font-weight: bold;">
+      Association IA Pau
+    </h1>
+    <iapau-button hoverColors mode="secondary" style="margin: 30px 0%;"
+      ><a href="/contact" class="link">Prendre contact</a></iapau-button
+    >
+  </div>
+  <div
+    style="height: 50px; width: 100%; transform: scale(-1,1); background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSI1MHB4IiB2aWV3Qm94PSIwIDAgMTI4MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0iI2ZmZmZmZiI+PHBhdGggZD0iTTEyODAgMGwtMjYyLjEgMTE2LjI2YTczLjI5IDczLjI5IDAgMCAxLTM5LjA5IDZMMCAwdjE0MGgxMjgweiIvPjwvZz48L3N2Zz4=);
+background-size: 100% 50px;"
+  />
+</div>
+<div style="height: 100vh" />
